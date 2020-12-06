ad812a36fe68614958a0c0a5aecfa5eb6a433f0c
Siddharth Saha <sahasiddharth611@gmail.com>
2020-12-06 23:04:18 +0530

    Helper functions script(missed adding it in previous commit)

---
+++ helper_functions.py
@@ -0,0 +1,23 @@
+# trunc8 did this
+import copy
+import numpy as np
+
+def deep_copy_params(to_call):
+  def f(*args, **kwargs):
+    return to_call(*(copy.deepcopy(x) for x in args),
+                   **{k: copy.deepcopy(v) for k, v in kwargs.items()})
+  return f
+
+
+def findLastNonZeroElement(lst):
+  return [index for index, item in enumerate(lst) if item != 0][-1]
+
+
+def findFirstNonZeroElement(lst):
+  return [index for index, item in enumerate(lst) if item != 0][0]
+
+
+@deep_copy_params
+def crop(image):
+  y_nonzero, x_nonzero = np.nonzero(image)
+  return image[np.min(y_nonzero):np.max(y_nonzero), np.min(x_nonzero):np.max(x_nonzero)]
